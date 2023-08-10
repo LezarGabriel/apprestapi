cbb531a9f61073f1d4c24a3796ad5a462e28cf51
DESKTOP-C3J5RM7\LENOVO <lezargabrieldarapai@gmail.com>
2023-08-10 15:47:13 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -6,3 +6,29 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("applikasi REST API ku berjalan!",res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+        }else {
+            response.ok(rows, res);
+            }
+                
+        });
+        
+};
